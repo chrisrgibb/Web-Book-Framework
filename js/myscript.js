8ce846b627695fd4a6e2234f2bda28a75e80f624
Chris Gibb <chrisrgibb@gmail.com>
2013-09-04 17:51:16 +1200

    changed a few variables

--- js/myscript.js
+++ js/myscript.js
@@ -1,20 +1,21 @@
 $(document).ready(function() {
   // Preload Images
   var preloadImages = ['images/page1.jpg',
-  					   'images/page2.jpg',
-  					   'images/page3.jpg',
-  					   'images/page4.jpg',
-  					   'images/page5.jpg'  ];
+  					           'images/page2.jpg',
+  					           'images/page3.jpg',
+  					           'images/page4.jpg',
+  					           'images/page5.jpg'  ];
 
   var preloadarrows = ['images/nextpage.png',
-                            'images/previouspage.png'];
+                       'images/previouspage.png'];
 
   // Store preloaded images in new Array             
                 
   var imgs = [];
-  var count = 0;
+  var currentPage = 0;
+  var numberOfPages = preloadImages.length;
 
-  for (var i=0; i<preloadImages.length;i++){
+  for (var i=0; i < numberOfPages;i++){
   	imgs[i] = new Image();
   	imgs[i].src = preloadImages[i];
   }
@@ -22,38 +23,27 @@ $(document).ready(function() {
   var newPhoto = new Image();
   newPhoto.src = preloadImages[0];
 
-
   var previousPage$ = $('#previous-page');
   var nextPage$ = $('#next-page');
 
-  if(count==0){
+  if(currentPage==0){
     previousPage$.hide();
   }else{
     previousPage$.show();
   }
 
-
-
   // Check for click to change picture
   $('#image').click(function(){
-
-
 
   }); // end click function
 
 $("body").keydown(function (e){
   // press left key
-  if(e.keyCode == 37 && count > 0){ // left
+  if(e.keyCode == 37 && currentPage > 0){ // left
     gotoPreviousPage();
-
-    // $('#image').attr('src', imgs[count].src);
   // press right key
-  }else if(e.keyCode == 39 && count<imgs.length-1){
+  }else if(e.keyCode == 39 && currentPage<numberOfPages-1){
      gotoNextPage();
-
-    // $('#image').attr('src', imgs[count].src);
-  
-
   }
 }); // end keydown function
 
@@ -65,46 +55,43 @@ $('#next-page').click(function(){
 
 $('#previous-page').click(function() {
   gotoPreviousPage();
-
 });  // end previous-page click function 
 
 
 function gotoPreviousPage(){
     console.log("clicked on yes!!!!");
-   if (count>0){
-      count--;
-      $('#image').attr('src', imgs[count].src);
+   if (currentPage>0){
+      currentPage--;
+      $('#image').attr('src', imgs[currentPage].src);
     }
     else{
       alert('too far!!!');
     }
 
-    if(count<imgs.length-1 && count > 0){
+    if(currentPage<numberOfPages-1 && currentPage > 0){
       nextPage$.show();
-    }else if (count==0) {
+    }else if (currentPage==0) {
     previousPage$.hide();
     }
 }
 
 function gotoNextPage(){
   console.log("Next PAGE!!!");
-    if (count<imgs.length-1){
-      count++;
-      $('#image').attr('src', imgs[count].src);
+    if (currentPage<numberOfPages-1){
+      currentPage++;
+      $('#image').attr('src', imgs[currentPage].src);
     }
     else{
       alert('too far!!!');
     }
-      if(count>=imgs.length-1){
+      if(currentPage>=numberOfPages-1){
     nextPage$.hide();
     }else{
     nextPage$.show();
   }
-
-    if(count>0){
+    if(currentPage>0){
       previousPage$.show();
     }
-
 }
 
 
